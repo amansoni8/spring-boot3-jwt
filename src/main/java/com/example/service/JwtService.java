1fbbc8fe05a20b5067514c21e9560d9fc08692ef
aman <amansoni22103@gmail.com>
2024-03-24 21:36:33 +0530

    new commit

--- src/main/java/com/example/service/JwtService.java
+++ src/main/java/com/example/service/JwtService.java
@@ -1,5 +1,37 @@
 package com.example.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.dto.Product;
+import com.example.repository.UserInfoRepository;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
 public class JwtService {
+
+	 List<Product> productList = null;
+
+	    @Autowired
+	    private UserInfoRepository repository;
+
+	    @Autowired
+	    private PasswordEncoder passwordEncoder;
+	    
+	    @PostConstruct
+	    public void loadProductsFromDB() {
+	        productList = IntStream.rangeClosed(1, 100)
+	                .mapToObj(i -> Product.builder()
+	                        .productId(i)
+	                        .name("product " + i)
+	                        .qty(new Random().nextInt(10))
+	                        .price(new Random().nextInt(5000)).build()
+	                ).collect(Collectors.toList());
+	    }
 
 }
