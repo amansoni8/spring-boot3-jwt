69657c6639773032082ee20432ecd159a4ab2fed
aman <amansoni22103@gmail.com>
2024-03-25 19:01:14 +0530

    new commit

--- src/main/java/com/example/service/JwtService.java
+++ src/main/java/com/example/service/JwtService.java
@@ -1,37 +1,70 @@
 package com.example.service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
-import com.example.dto.Product;
-import com.example.repository.UserInfoRepository;
-
-import jakarta.annotation.PostConstruct;
-
-@Service
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+@Component
 public class JwtService {
 
-	 List<Product> productList = null;
-
-	    @Autowired
-	    private UserInfoRepository repository;
-
-	    @Autowired
-	    private PasswordEncoder passwordEncoder;
-	    
-	    @PostConstruct
-	    public void loadProductsFromDB() {
-	        productList = IntStream.rangeClosed(1, 100)
-	                .mapToObj(i -> Product.builder()
-	                        .productId(i)
-	                        .name("product " + i)
-	                        .qty(new Random().nextInt(10))
-	                        .price(new Random().nextInt(5000)).build()
-	                ).collect(Collectors.toList());
-	    }
-
+	public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
+	
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+	
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+	
+	private Claims extractAllClaims(String token) {
+		return Jwts .parserBuilder() 
+				.setSigningKey(getSignKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
+		
+	}
+	private Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+	
+	public Boolean validateToken(String token , UserDetails userDetails) {
+		final String username = extractUsername(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+	}
+	
+	private String createToken(Map<String,Object> claims, String userName) {
+		return Jwts.builder()
+				.setClaims(claims)
+				.setSubject(userName)
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis()+1000*60*30))
+				.signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+	}
+	
+	public String generateToken(String userName) {
+		Map<String,Object> claims =  new HashMap<>();
+		return createToken(claims,userName);
+	}
+	
+	private Key getSignKey() {
+		byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
 }
