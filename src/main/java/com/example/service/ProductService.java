a58e5361c6ec09d428a77ac3a9ae2d02c1aaf4b5
aman <amansoni22103@gmail.com>
2024-03-29 20:05:50 +0530

    new commit

--- src/main/java/com/example/service/ProductService.java
+++ src/main/java/com/example/service/ProductService.java
@@ -1,11 +1,16 @@
 package com.example.service;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.dto.Product;
+import com.example.entity.UserInfo;
 import com.example.repository.UserInfoRepository;
 
 import jakarta.annotation.PostConstruct;
@@ -13,16 +18,41 @@ import jakarta.annotation.PostConstruct;
 @Service
 public class ProductService {
 	
-	List<Product> productList = null;
-	
-    @Autowired
-    private UserInfoRepository repository;
+	 List<Product> productList = null;
 
-//    @Autowired
-//    private final PasswordEncoder passwordEncoder;
-    
-    @PostConstruct
-    public void loadProductsFromDB() {
-    	
-    	}
+	    @Autowired
+	    private UserInfoRepository repository;
+
+	    @Autowired
+	    private PasswordEncoder passwordEncoder;
+
+	    @PostConstruct
+	    public void loadProductsFromDB() {
+	        productList = IntStream.rangeClosed(1, 100)
+	                .mapToObj(i -> Product.builder()
+	                        .productId(i)
+	                        .name("product " + i)
+	                        .qty(new Random().nextInt(10))
+	                        .price(new Random().nextInt(5000)).build()
+	                ).collect(Collectors.toList());
+	    }
+
+
+	    public List<Product> getProducts() {
+	        return productList;
+	    }
+
+	    public Product getProduct(int id) {
+	        return productList.stream()
+	                .filter(product -> product.getProductId() == id)
+	                .findAny()
+	                .orElseThrow(() -> new RuntimeException("product " + id + " not found"));
+	    }
+
+
+	    public String addUser(UserInfo userInfo) {
+	        userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+	        repository.save(userInfo);
+	        return "user added to system ";
+	    }
 }
